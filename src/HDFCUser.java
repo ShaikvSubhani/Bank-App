f4d66382dc7260d9186b5d93c75d402ba672f190
ShaikvSubhani <shaik4321subhani@gmail.com>
2023-01-12 12:13:42 +0530

    HDFCUser

--- src/HDFCUser.java
+++ src/HDFCUser.java
@@ -1,3 +1,95 @@
-public class HDFCUser {
+import java.util.Objects;
+import java.util.UUID;
 
+public class HDFCUser implements BankInterface{
+       private String name;
+       private String accountNo;
+       private double balance;
+       private String password;
+       private static double rateOfInterest;
+
+    public HDFCUser(String name, String password,double balance) {
+        this.name = name;
+        this.balance = balance;
+        this.password = password;
+
+        this.rateOfInterest=7;
+
+        this.accountNo=String.valueOf(UUID.randomUUID());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public static double getRateOfInterest() {
+        return rateOfInterest;
+    }
+
+    public static void setRateOfInterest(double rateOfInterest) {
+        HDFCUser.rateOfInterest = rateOfInterest;
+    }
+
+    @Override
+
+    public double checkBalance() {
+        return this.balance;
+    }
+
+    @Override
+    public String addMoney(int amount) {
+        balance = balance + amount;
+        return "Successfully added";
+    }
+
+    @Override
+    public String withdrawMoney(int amount, String enteredPassword) {
+        if(Objects.equals(password,enteredPassword))
+        {
+            if(amount>balance)
+                return "paisa nahi h utna, abhi bank m itna h  "+balance;
+            else {
+                balance = balance - amount;
+                return "bahut paisa h, lekin dekhke kharch kar "+ balance;
+            }
+        }
+        else
+            return "wrong password";
+
+    }
+
+    @Override
+    public double calculateInterest(int years) {
+        double interest= (balance * years * rateOfInterest)/100;
+
+        return balance + interest;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
@@ -7,21 +8,47 @@ public class Main {
         String name =sc.next();
         String password=sc.next();
         double balance=sc.nextDouble();
-        SBIUser user=new SBIUser(name,balance,password);
+//        SBIUser user=new SBIUser(name,balance,password);
+//
+//        //add amount
+//        String msg=user.addMoney(100000);
+//        System.out.println(msg);
+//
+//        //withdraw money
+//        System.out.println("enter amount you want to withdraw");
+//        int  money=sc.nextInt();
+//        System.out.println("enter your password");
+//        String pass=sc.next();
+//        System.out.println(user.withdrawMoney(money,pass));
+//
+//        //rate of interest
+//        System.out.println(user.calculateInterest(10));
 
-        //add amount
-        String msg=user.addMoney(100000);
+        HDFCUser user=new HDFCUser(name,password,balance);
+
+        //aadding the amount
+        System.out.println("Enter the amount of money which you want to add");
+        int amount=sc.nextInt();
+        String msg= user.addMoney(amount);
         System.out.println(msg);
 
-        //withdraw money
-        System.out.println("enter amount you want to withdraw");
-        int  money=sc.nextInt();
-        System.out.println("enter your password");
-        String pass=sc.next();
-        System.out.println(user.withdrawMoney(money,pass));
+        //checking balance
+        System.out.println(user.checkBalance());
 
-        //rate of interest
+        //to withdraw money;
+        System.out.println("Enter the amount which you want to withdram");
+         amount=sc.nextInt();
+        System.out.println(user.withdrawMoney(amount,password));
+
+        //calculating total balance with interest;
         System.out.println(user.calculateInterest(10));
+
+
+
+
+
+
+
 
     }
 }
